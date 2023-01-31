fdca73c69b34c2b8e2f37295b5b11b0a5b528ae6
soyRicardoCastro <ricardocastro.dev@gmail.com>
2023-01-31 14:54:32 -0500

    new logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
+import preact from "@astrojs/preact";
+import tailwind from "@astrojs/tailwind";
+import astroI18next from "astro-i18next";
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
-import preact from "@astrojs/preact";
-
-import astroI18next from "astro-i18next";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact(), astroI18next()]
+  integrations: [
+		tailwind(),
+		preact(),
+		astroI18next(),
+	]
 });
